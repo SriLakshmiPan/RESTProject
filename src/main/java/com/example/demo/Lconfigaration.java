551507942072561e6e217a74cc6f3fb670877028
SriLakshmiPan <SriLakshmiPan@gmail.com>
2020-08-28 11:39:46 -0400

    intial changes

---
+++ src/main/java/com/example/demo/Lconfigaration.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Lconfigaration{ 
+  @Bean
+  public DataSource source(){
+    DataSource source = new OracleDataSource();
+    source.setURL();
+    source.setUser();
+    return source;
+  }
+  @Bean
+  public PlatformTransactionManager manager(){
+    PlatformTransactionManager manager = new BasicDataSourceTransactionManager();
+    manager.setDataSource(source());
+    return manager;
+  }
+}
